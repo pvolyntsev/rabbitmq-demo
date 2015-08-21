9bd9dc29d31fe5050c4d0de25167b6e4147c6095
Pavel Volyntsev <pvolyntsev@users.noreply.github.com>
2015-08-21 17:32:15 +0600

    Reverted connect to queue when new socket connected

--- js/server.js
+++ js/server.js
@@ -6,8 +6,6 @@ var context = require('rabbit.js').createContext(),
 
 // Set correct encoding.
 sub.setEncoding('utf8');
-// Connect socket to updates exchange.
-sub.connect('updates');
 
 // A websocket is connected (eg: browser).
 io.sockets.on('connection', function(socket) {
@@ -16,7 +14,10 @@ io.sockets.on('connection', function(socket) {
         var message = JSON.parse(data);
         socket.emit(message.type, message.data);
     }
-    
+
+    // Connect socket to updates exchange.
+    sub.connect('updates');
+
     // Register handler that hanles incoming data when the socket
     // detects new data on our queues.
     // When receiving data, it gets pushed to the connected websocket.
