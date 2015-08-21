10966c7bafc6bff65c279cae9dc33a348bc5d44c
Pavel Volyntsev <pvolyntsev@users.noreply.github.com>
2015-08-21 17:28:06 +0600

    Unsubscribe disconnected websocket from queue

--- js/server.js
+++ js/server.js
@@ -6,18 +6,25 @@ var context = require('rabbit.js').createContext(),
 
 // Set correct encoding.
 sub.setEncoding('utf8');
+// Connect socket to updates exchange.
+sub.connect('updates');
 
 // A websocket is connected (eg: browser).
 io.sockets.on('connection', function(socket) {
 
-    // Connect socket to updates exchange.
-    sub.connect('updates');
-
-    // Register handler that hanles incoming data when the socket
-    // detects new data on our queues.
-    // When receiving data, it gets pushed to the connectec websocket.
-    sub.on('data', function(data) {
+    function pushDataToSocket(data) {
         var message = JSON.parse(data);
         socket.emit(message.type, message.data);
+    }
+    
+    // Register handler that hanles incoming data when the socket
+    // detects new data on our queues.
+    // When receiving data, it gets pushed to the connected websocket.
+    sub.on('data', pushDataToSocket);
+    
+    // Unsubscribe disconnected websocket from queue 
+    socket.on('disconnect', function(){
+       sub.removeListener('data', pushDataToSocket);
     });
+
 });
